e023769e0ca93ce88398d6025414bf83d95d98c0
BenjaminRousseau <rousseau.j.benjamin@gmail.com>
2015-11-06 14:52:39 +0100

    Gestion Singleton IHM et EditeurGraphique

--- src/Vues/EditeurGraphique.java
+++ src/Vues/EditeurGraphique.java
@@ -1,21 +1,23 @@
 package Vues;
-import javax.swing.*;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.Toolkit;
 
-import com.company.Controlleur;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+import Controleurs.ControleurDiagramme;
+
 import com.mxgraph.view.mxGraph;
 
 /**
  * Created by Jerem on 11/10/2015.
  */
-public class EditeurGraphique extends JFrame implements ObservateurVue {
+public class EditeurGraphique extends JFrame implements Observateur {
 
-    Toolkit tool = getToolkit();
-
-    private mxGraph graph = new mxGraph();
-    private ControleurDiagramme controleur = null;
+	final private static EditeurGraphique instanceUnique = new EditeurGraphique();
 
     //  Partie Barre de menu
     private JMenuBar menu = new JMenuBar();
@@ -29,45 +31,59 @@ public class EditeurGraphique extends JFrame implements ObservateurVue {
 
     private JMenuItem consulter_manuel = new JMenuItem("Consulter Manuel");
     private JMenuItem a_propos = new JMenuItem("A Propos");
+	
+	private EditeurGraphique() {		
+		this.setTitle("Editeur Graphique");
+		this.setVisible(true);
+		this.setSize(tool.getScreenSize());
+		
+		// Ajout des sous-items aux items de la barre de menu
+		fichier.add(ouvrir);
+		fichier.add(enregistrer);
+		fichier.addSeparator();
+		fermer.addActionListener(new ActionListener() {
+		    @Override
+		    public void actionPerformed(ActionEvent e) {
+		        System.exit(0);
+		    }
+		});
+		fichier.add(fermer);
+		
+		aide.add(consulter_manuel);
+		aide.add(a_propos);
+		
+		menu.add(fichier);
+		menu.add(aide);
+		
+		this.setJMenuBar(menu);
+		
+		this.addMouseListener(new MenuContextuelListener());
+	}
+	
+	static public EditeurGraphique instance() {
+		return instanceUnique;
+	}
+	
+    Toolkit tool = getToolkit();
 
-    public EditeurGraphique(ControleurDiagramme controleur){
-        this.controleur = controleur;
-        this.setTitle("Editeur Graphique");
-        this.setVisible(true);
-        this.setSize(tool.getScreenSize());
+    private mxGraph graph = new mxGraph();
 
-        // Ajout des sous-items aux items de la barre de menu
-        fichier.add(ouvrir);
-        fichier.add(enregistrer);
-        fichier.addSeparator();
-        fermer.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
-        fichier.add(fermer);
 
-        aide.add(consulter_manuel);
-        aide.add(a_propos);
 
-        menu.add(fichier);
-        menu.add(aide);
-
-        this.setJMenuBar(menu);
-
-        this.addMouseListener(new MenuContextuelListener());
-
-    }
-
-    public void ajoutEtat(){
+  /*  public void ajoutEtat(){
         CreationEtat window_etat = new CreationEtat();
         //window_etat.getValider().
     }
 
     public ControleurDiagramme getControleur(){
         return controleur;
-    }
+    }*/
+
+	@Override
+	public void miseAJour() {
+		// TODO Auto-generated method stub
+		
+	}
 
     /*void miseAjour(){
 
--- src/Vues/Ihm.java
+++ src/Vues/Ihm.java
@@ -1,14 +1,26 @@
 package Vues;
 
-import ElementsDiagramme.Element;
 import ElementsDiagramme.Etat;
 import ElementsDiagramme.Transition;
 
 /**
- * Created by rémy on 06/11/2015.
+ * Created by rï¿½my on 06/11/2015.
  */
 public class Ihm {
+	final private static Ihm instanceUnique = new Ihm();
+	private EditeurGraphique edGraphique = EditeurGraphique.instance();
 
+
+	private Ihm() {	}
+	
+	static public Ihm instance() {
+		return instanceUnique;
+	}
+
+	public EditeurGraphique getEdGraphique() {
+		return this.edGraphique;
+	}
+	
     public TransitionGraph createTransitionGraph(Transition t){
 
         return null;
