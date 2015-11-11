c396f762e7995ec14912a1bea79cbf5f7c16622f
Jerem28 <Jerem28@users.noreply.github.com>
2015-11-11 23:34:10 +0100

    Zone erreur updateLabel

--- src/Vues/ZoneErreur.java
+++ src/Vues/ZoneErreur.java
@@ -5,6 +5,7 @@ import Erreurs.Erreur;
 import javax.swing.*;
 import java.awt.*;
 import java.util.HashSet;
+import java.util.Iterator;
 
 /**
  * Created by Jerem on 06/11/2015.
@@ -21,7 +22,11 @@ public class ZoneErreur extends JPanel implements ObservateurVue {
 
     public void miseAJour(){
         HashSet<Erreur> erreurs = Ihm.instance().getControleur().getErreurs();
-        //for(Hash)
+        String error_text = "";
+        for(Erreur e : erreurs){
+            error_text.concat(e.getNom());
+        }
+        zone_texte.setText(error_text);
     }
 
 }
